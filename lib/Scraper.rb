85bcb72ece167bac30496e52e01c927eabcc13ec
Sara Stanton <sara.mpa13@gmail.com>
2017-11-11 22:47:08 +0000

    added scraping notes

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -7,12 +7,14 @@ class Scraper
   def self.scrape_knitpicks
     doc = Nokogiri::HTML(open('http://www.knitpicks.com/yarns/All_Knit_Picks_Yarn__L300198.html'))
     binding.pry
-    # @name =
-    # @weight =
-    # @fiber =
-    # @price =
+    # @name = doc.css(".titleSmall").first.attribute("title").value
+    # @weight = doc.css(".yarnWeight")[1].text
+    # @fiber = doc.css(".yarnWeight").first.text
+    # @price = doc.css(".costSmall").first.text.strip   **maybe parse to separate $ from grams?**
     # @size =
     # @yardage =
+
+# should it have one class scrape the "All yarns" page and then another scraping the url of each individual yarn?
 
   end
 
