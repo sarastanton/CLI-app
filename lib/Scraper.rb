e113a21e1d8a2e199318f1532ccf4907c0e91335
Sara Stanton <sara.mpa13@gmail.com>
2017-11-13 06:28:06 +0000

    split scraper methods back to 1

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -8,8 +8,14 @@ class Scraper
 
   def self.scrape_knitpicks
     doc = Nokogiri::HTML(open('http://www.knitpicks.com/yarns/All_Knit_Picks_Yarn__L300198.html'))
-    doc.css(".titleSmall").each do |url|
-       @@yarn_urls << url.attribute("href").value
+    binding.pry
+    @name = doc.css(".titleSmall").first.attribute("title").value
+    @weight = doc.css(".yarnWeight")[1].text
+    @fiber = doc.css(".yarnWeight").first.text
+    @price = doc.css(".costSmall").first.text.strip
+      if doc.css(".listItemStatusTags").text == "SaleSale"
+        @sale = true
+        else @sale = false
       end
   end
 
@@ -18,27 +24,25 @@ class Scraper
 # should it have one class scrape the "All yarns" page and then another scraping the url of each individual yarn?
 
 
-  def self.scrape_details
-  #   @@yarn_urls.each do |url|
-  #     doc = Nokogiri::HTML(open(url)
-  #    #   binding.pry
-  # #   # @name = doc.css(".titleSmall").first.attribute("title").value
-  #     @name = doc.css(".prodTitle").text.strip
-  # #   @weight =
-  # #   @fiber =
-  # #   @price =
-  # #   @grams =
-  # #   @yardage =
-  #   # sale? = nil
-  #   # if ___
-  #   #   sale? = true
-  #   # end
-  #   end
-  #
-  #   # @size =
-    # @yardage =
+  # def self.scrape_details
+  #   doc = Nokogiri::HTML(open("http://www.knitpicks.com/yarns/Swish_Worsted_Yarn__D5420153.html")) #***CHANGE BACK!**
+  #     # @name = doc.css(".prodTitle").text.strip
+  #     # @weight = doc.css(".prodTitle").attribute("href").value
+  #     # @fiber = doc.css("")
+  # # #   @price =
+  # # #   @grams =
+  # # #   @yardage =
+  # #   # sale? = nil
+  # #   # if ___
+  # #   #   sale? = true
+  # binding.pry
 
-
+#     end
+#   #
+#   #   # @size =
+#     # @yardage =
+#
+# self.scrape_details
 
 
   end
