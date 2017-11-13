d5ff420165be03572fb8fbfc7ff32fe56dfb3e81
Sara Stanton <sara.mpa13@gmail.com>
2017-11-13 05:28:29 +0000

    split out scraper method into 2

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -4,21 +4,46 @@ require 'pry'
 
 class Scraper
 
+@@yarn_urls = []
+
   def self.scrape_knitpicks
     doc = Nokogiri::HTML(open('http://www.knitpicks.com/yarns/All_Knit_Picks_Yarn__L300198.html'))
-    binding.pry
-    # @name = doc.css(".titleSmall").first.attribute("title").value
-    # @weight = doc.css(".yarnWeight")[1].text
-    # @fiber = doc.css(".yarnWeight").first.text
-    # @price = doc.css(".costSmall").first.text.strip   **maybe parse to separate $ from grams?**
-    # @size =
-    # @yardage =
-
-# should it have one class scrape the "All yarns" page and then another scraping the url of each individual yarn?
-
+    doc.css(".titleSmall").each do |url|
+       @@yarn_urls << url.attribute("href").value
+      end
   end
 
   self.scrape_knitpicks
 
+# should it have one class scrape the "All yarns" page and then another scraping the url of each individual yarn?
 
-end
+
+  def self.scrape_details
+  #   @@yarn_urls.each do |url|
+  #     doc = Nokogiri::HTML(open(url)
+  #    #   binding.pry
+  # #   # @name = doc.css(".titleSmall").first.attribute("title").value
+  #     @name = doc.css(".prodTitle").text.strip
+  # #   @weight =
+  # #   @fiber =
+  # #   @price =
+  # #   @grams =
+  # #   @yardage =
+  #   # sale? = nil
+  #   # if ___
+  #   #   sale? = true
+  #   # end
+  #   end
+  #
+  #   # @size =
+    # @yardage =
+
+
+
+
+  end
+
+
+# @weight = doc.css(".yarnWeight")[1].text
+# @fiber = doc.css(".yarnWeight").first.text
+# @price = doc.css(".costSmall").first.text.strip   **maybe parse to separate $ from grams?**
--- lib/Yarn.rb
+++ lib/Yarn.rb
@@ -1,16 +1,32 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Yarn
-attr_accessor :name, :weight, :fiber, :price, :size, :yardage, :sale
+  attr_accessor :name, :weight, :fiber, :price, :sale
 
-def initialize(sale=nil)
-  @name = name
-  @weight = weight
-  @fiber = fiber
-  @price = price
-  @size = size
-  @yardage = yardage
-end
+  @@yarns = []
 
+  def initialize(sale = nil)
+    @@yarns << self
+    @sale = sale
+  end
 
+  def self.new_from_website
+    doc = Nokogiri::HTML(open('http://www.knitpicks.com/yarns/All_Knit_Picks_Yarn__L300198.html'))
 
+    doc.css(".titleSmall").each do |y1|
+      y1 = Yarn.new
+      Yarn.name =
+      Yarn.weight = doc.css(".yarnWeight")[1].text
+      Yarn.fiber = doc.css(".yarnWeight").first.text
+      Yarn.price = doc.css(".costSmall").first.text.strip
+      @@yarns << y1
+      end
+      binding.pry
 
+    yarn.yarn_url = @yarn_url = "www.knitpicks.com#{doc.css(".titleSmall").first.attribute("href").value}"
+  end
+
+  self.new_from_website
 end
