be40038d235f6976f96d8f594a9d5b1a3c24b249
Sara Stanton <sara.mpa13@gmail.com>
2018-02-12 03:13:09 +0000

    cleaned up for Gem release

--- lib/knitpickr.rb
+++ lib/knitpickr.rb
@@ -1,10 +1,10 @@
 require 'open-uri'
 require 'nokogiri'
 
+module Knitpickr
+end
+
 require_relative './knitpickr/controller'
 require_relative './knitpickr/scraper'
 require_relative './knitpickr/version'
 require_relative './knitpickr/yarn'
-
-module Knitpickr
-end
